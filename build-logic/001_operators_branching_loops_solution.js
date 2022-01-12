660df7ebe4238689be96d3f6b151e2866b01b61e
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-01-12 14:57:27 +0530

    Update - solved upto q3 in 001.md
    1. Added a JS file
    2. Installed a few extensions which helped me in debugging well
    3. Understood how Terminal works
    4. Installed node module as there was a missing package for the js file to execute
    5. Refactored the code to ES6+

---
+++ build-logic/001_operators_branching_loops_solution.js
@@ -0,0 +1,33 @@
+//Easy
+// Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+
+let sum = (num1, num2, num3, num4, num5) => num1 + num2 + num3 + num4 + num5;
+
+console.log(sum(5, 13, 7, 21, 48));
+
+// Write a program to take a number input from user and determine whether the number is odd or even.
+const readlineSync = require("readline-sync");
+const number = readlineSync.question("Enter a number -> ");
+
+number % 2 === 0 ? console.log("It is Odd") : console.log("It is Even");
+
+// Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+const minMax = (num1, num2) =>
+  num1 > num2
+    ? `Min = ${num2} and Min = ${num1}`
+    : `Min = ${num1} and Min = ${num2}`;
+
+console.log(minMax(129, 251));
+
+// Write a program to find the maximum out of three given numbers. The numbers are num1=8, num2=23 and num3=17.
+const maxNum = (num1, num2, num3) => {
+  return num1 > num2 && num1 > num3
+    ? `${num1} is the maximum`
+    : num2 > num3
+    ? `${num2} is maximum`
+    : `${num3} is maximum`;
+};
+
+console.log(maxNum(8, 23, 17));
+
+//Write a program to find the minimum out of three given numbers. The numbers are num1=35, num2=29 and num3=46.
